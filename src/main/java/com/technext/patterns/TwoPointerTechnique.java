a22185ab773cc0a6cf0a58c232dc823d1580c4a9
ggiande <ggarciadeleon@csustan.edu>
2021-11-07 12:42:42 -0800

    Added sourc code for two-pointer technique Square Each Value problem and test case

--- src/main/java/com/technext/patterns/TwoPointerTechnique.java
+++ src/main/java/com/technext/patterns/TwoPointerTechnique.java
@@ -1,7 +1,6 @@
 package com.technext.patterns;
 
-/** The Two Pointer Technique ideally runs
- * in a linear environment. */
+/** The Two Pointer Technique ideally runs in a linear environment. */
 public class TwoPointerTechnique {
   /** Two Sum is a famous LeetCode question. This approach
    * returns the two indexes that match a target given
@@ -24,5 +23,57 @@ public class TwoPointerTechnique {
       }
     }
     return new int[]{-1, -1};
+  }
+
+  /** Squaring Values is a famous LeetCode question. This method
+   * takes in an array of sorted elements and returns
+   * a sorted array of the squared values of the original elements.
+   * Solution:
+   * RightPointer = 1st non-neg value
+   * LeftPointer = Left of RightPointer
+   * Constraints -231 <= x <= 231 - 1
+   *
+   * @param sortedArray a sorted array of integers
+   * @return an array of sorted integers whose values are squared */
+  public int[] squareEachValue(final int[] sortedArray) {
+
+    int rightPointer = 0;
+
+    while (rightPointer < sortedArray.length && sortedArray[rightPointer] < 0) {
+      rightPointer += 1;
+    }
+    int leftPointer = rightPointer - 1;
+
+    int[] sortedSquaredArray = new int[sortedArray.length];
+    int currentIndexInArray = 0;
+
+    while (leftPointer >= 0 && rightPointer < sortedArray.length) {
+      int leftPointerValue = sortedArray[leftPointer] * sortedArray[leftPointer];
+      int rightPointerValue = sortedArray[rightPointer] * sortedArray[rightPointer];
+
+      if (leftPointerValue < rightPointerValue) {
+        sortedSquaredArray[currentIndexInArray] = leftPointerValue;
+        leftPointer--;
+      } else {
+        sortedSquaredArray[currentIndexInArray] = rightPointerValue;
+        rightPointer++;
+      }
+      currentIndexInArray++;
+    }
+    while (leftPointer >= 0) {
+      int leftPointerValue = sortedArray[leftPointer] * sortedArray[leftPointer];
+
+      sortedSquaredArray[currentIndexInArray] = leftPointerValue;
+      leftPointer--;
+      currentIndexInArray++;
+    }
+    while (rightPointer < sortedArray.length) {
+      int rightPointerValue = sortedArray[rightPointer] * sortedArray[rightPointer];
+
+      sortedSquaredArray[currentIndexInArray] = rightPointerValue;
+      rightPointer++;
+      currentIndexInArray++;
+    }
+    return sortedSquaredArray;
   }
 }
--- src/test/java/com/technext/patterns/TwoPointerTechniqueTest.java
+++ src/test/java/com/technext/patterns/TwoPointerTechniqueTest.java
@@ -8,9 +8,14 @@ public class TwoPointerTechniqueTest extends TestCase {
 
   TwoPointerTechnique twoPointerTechnique;
 
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    twoPointerTechnique = new TwoPointerTechnique();
+  }
+
   @Test
   public void twoSum() {
-    twoPointerTechnique = new TwoPointerTechnique();
     int[] arrAddsUp = new int[]{2, 7, 11, 15};
     int addsUpTarget = 9;
     int[] expectedAddsUp = new int[]{0, 1};
@@ -19,10 +24,16 @@ public class TwoPointerTechniqueTest extends TestCase {
 
   @Test
   public void twoSumImpossibleTarget() {
-    twoPointerTechnique = new TwoPointerTechnique();
     int[] doesNotAddUpArr = new int[]{2, 7, 11, 15};
     int doesNotAddUpTarget = 40;
     int[] expectedDoesNotAddUpArr = new int[]{-1, -1};
     assertArrayEquals(expectedDoesNotAddUpArr, twoPointerTechnique.twoSum(doesNotAddUpArr, doesNotAddUpTarget));
+  }
+
+  @Test
+  public void testSquareEachValue() {
+    int[] sortedArray = new int[]{-4, -3, 1, 2, 3};
+    int[] squaredSortedArray = new int[]{1, 4, 9, 9, 16};
+    assertArrayEquals(squaredSortedArray, twoPointerTechnique.squareEachValue(sortedArray));
   }
 }
