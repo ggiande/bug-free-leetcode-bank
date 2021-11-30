e96952f46f3ce4a87cec2a8ac7ed180cfc6b4d9f
Giancarlo <ggarciadeleon@csustan.edu>
2021-11-30 11:23:00 -0800

    added edge case, changed set values to the Node itself, no longer needs unique values

--- src/main/java/com/technext/patterns/SinglyCycleDetection.java
+++ src/main/java/com/technext/patterns/SinglyCycleDetection.java
@@ -14,17 +14,21 @@ public class SinglyCycleDetection {
      * @param head The Singly Linked List Node
      * @return boolean stating whether a cycle was found in the head */
     public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+
         ListNode curr = head;
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<ListNode> set = new HashSet<>();
 
         // traverse through our linked list
         while(curr != null) {
             // search for the cycle
-            if(set.contains(curr.val)) {
+            if(set.contains(curr)) {
                 return Boolean.TRUE;
             }
             // if not in cycle, then add it to the set
-            set.add(curr.val);
+            set.add(curr);
             //advance the current node to the next
             curr = curr.next;
         }
